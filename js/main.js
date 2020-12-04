ac2481617ba544445d26923a0f3980c4557f1f0f
Sarthak Shah <sharthakshah81173@gmail.com>
2020-12-04 11:07:20 -0500

    added database

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-const navToogle = document.querySelector('.nav-toggle');
+import { fetchData, postData } from "./components/TheDataMiner.js";
+(() => {
+// Hamburger menu
+    const navToogle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav_link')
 
 navToogle.addEventListener('click', () => {
@@ -10,38 +13,69 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+// end of hamburger menu
 
 
 
 
 
+// vue bgins
+    Vue.component("thumbnail-card",{
+        props: ["item"],
+        template: `<img  @click="logClicked" :src="'images/' + item.image" alt="item images">`,
 
+        mounted:function(){
+            console.log(`loaded a ${this.item.name}'s image`);
+            // console.log(`${this.item.image}`);
+        },
 
+        methods:{
+            logClicked(){
+                console.log("Car image Selected", this.item.name);
+                this.$emit("showmydata", this.item)
+            }
+            
+        }
+    });
 
+    let vue_vm = new Vue({
+        // link Vue to an element in our HTML
+        //el: "#app",
 
+        data: {
+            message: "Hello from Vue!",
+            anotherMessage: "more text, so simple! much winning",
+            // removeAformat: true,
+            show_data: false,
+            carModels: [],
+            currentModelData: {}
+        },
 
+        // this is the "mounted" lifecycle hook. Vue is done creating itself, and has attached itself to the "app" div on the page
+        mounted: function() {
+            console.log("Vue is mounted, trying a fetch for the initial data");
+            
+            fetchData("./includes/index.php")
+                .then(data => {
+                    data.forEach(prof => this.carModels.push(prof));
+                })
+                .catch(err => console.error(err));            
+        },
 
+    
 
+        methods: {
+            carThumbSelected(item){
+                console.log("clicked on a list item: ", item.name);
+                this.show_data = this.show_data ? false : true;
+                this.currentModelData = item;
+            }
+           
 
+            
+        }
 
-
-
-// (()=> {
-//     fetch('./data/AboutData.json')
-//     .then(res => res.json())
-//     .then(data => {
-
-//         aboutpage(data);
-//         debugger;
-//     })
-
-//     function aboutpage(data){
-//         let aboutTitle = document.querySelector('.AboutmeTitle p'),
-//             aboutText = document.querySelector('.AboutmeText p');
-
-//             aboutTitle.innerHTML = data.title;
-//             aboutText.innerHTML = data.text[0] + "<br><br>" + data.text[1];
-//     }
-
-
-// })();
+        
+    })  .$mount("#app");
+ // also connects Vue to your wrapper in HTML
+})();
